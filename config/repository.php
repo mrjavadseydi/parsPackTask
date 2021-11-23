6d8dc512d427fafb14a95f151244818534d21909
mohammad javad seydi <seydi.birjand@gmail.com>
2021-11-23 11:25:07 +0330

    add Directory Repository

--- config/repository.php
+++ config/repository.php
@@ -4,4 +4,5 @@ return [
     \App\Http\Lib\Interfaces\Auth\RegisterInterface::class => \App\Http\Lib\Repository\Auth\RegisterRepository::class,
     \App\Http\Lib\Interfaces\Auth\LogOutInterface::class => \App\Http\Lib\Repository\Auth\LogOutRepository::class,
     \App\Http\Lib\Interfaces\System\ProcessesInterface::class=>\App\Http\Lib\Repository\System\ProcessesRepository::class,
+    \App\Http\Lib\Interfaces\File\DirectoryInterface::class=>\App\Http\Lib\Repository\File\DirectoryRepository::class,
 ];
