c3b3b18db6c1e5345c424a0680a2321733598545
mohammad javad seydi <seydi.birjand@gmail.com>
2021-11-23 10:03:51 +0330

    add LogOut and Processes repository

--- config/repository.php
+++ config/repository.php
@@ -2,5 +2,6 @@
 return [
     \App\Http\Lib\Interfaces\Auth\LoginInterface::class => \App\Http\Lib\Repository\Auth\LoginRepository::class,
     \App\Http\Lib\Interfaces\Auth\RegisterInterface::class => \App\Http\Lib\Repository\Auth\RegisterRepository::class,
-    \App\Http\Lib\Interfaces\Auth\LogOutInterface::class => \App\Http\Lib\Repository\Auth\LogOutRepository::class
+    \App\Http\Lib\Interfaces\Auth\LogOutInterface::class => \App\Http\Lib\Repository\Auth\LogOutRepository::class,
+    \App\Http\Lib\Interfaces\System\ProcessesInterface::class=>\App\Http\Lib\Repository\System\ProcessesRepository::class,
 ];
