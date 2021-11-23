cd30e2c090e10050cb2b5b52ba5dce4e8a02d9dc
mohammad javad seydi <seydi.birjand@gmail.com>
2021-11-23 19:01:34 +0330

    add directoryList Repository to config

--- config/repository.php
+++ config/repository.php
@@ -5,4 +5,5 @@ return [
     \App\Http\Lib\Interfaces\Auth\LogOutInterface::class => \App\Http\Lib\Repository\Auth\LogOutRepository::class,
     \App\Http\Lib\Interfaces\System\ProcessesInterface::class=>\App\Http\Lib\Repository\System\ProcessesRepository::class,
     \App\Http\Lib\Interfaces\File\DirectoryInterface::class=>\App\Http\Lib\Repository\File\DirectoryRepository::class,
+    \App\Http\Lib\Interfaces\File\DirectoryListInterface::class=>\App\Http\Lib\Repository\File\DirectoryListRepository::class
 ];
