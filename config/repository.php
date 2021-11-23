b160c0444e325d9fddd397d4fa03baa7ab12bc04
mohammad javad seydi <seydi.birjand@gmail.com>
2021-11-23 19:19:13 +0330

    add fileRepository to config

--- config/repository.php
+++ config/repository.php
@@ -5,5 +5,6 @@ return [
     \App\Http\Lib\Interfaces\Auth\LogOutInterface::class => \App\Http\Lib\Repository\Auth\LogOutRepository::class,
     \App\Http\Lib\Interfaces\System\ProcessesInterface::class=>\App\Http\Lib\Repository\System\ProcessesRepository::class,
     \App\Http\Lib\Interfaces\File\DirectoryInterface::class=>\App\Http\Lib\Repository\File\DirectoryRepository::class,
-    \App\Http\Lib\Interfaces\File\DirectoryListInterface::class=>\App\Http\Lib\Repository\File\DirectoryListRepository::class
+    \App\Http\Lib\Interfaces\File\DirectoryListInterface::class=>\App\Http\Lib\Repository\File\DirectoryListRepository::class,
+    \App\Http\Lib\Interfaces\File\FileInterface::class=>\App\Http\Lib\Repository\File\FileRepository::class,
 ];
