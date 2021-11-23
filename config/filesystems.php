6a3dc1306c9f18db1a2a42b7c5861be086586348
mohammad javad seydi <seydi.birjand@gmail.com>
2021-11-23 11:20:57 +0330

    add myProgram driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,10 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'myProgram' => [
+            'driver' => 'local',
+            'root' => '/opt/myprogram/',
+        ],
     ],
 
     /*
