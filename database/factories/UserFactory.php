6599954cdc8f3a288448e9e55ce71dea2e9206fc
mohammad javad seydi <seydi.birjand@gmail.com>
2021-11-24 18:53:41 +0330

    update user factory for parspack user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Jobs\CreateUserDirectoryJob;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -14,11 +15,12 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $username = "parspack";
+        CreateUserDirectoryJob::dispatch($username);
         return [
             'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'username' => $username,
+            'password' => bcrypt('123456'), // password
             'remember_token' => Str::random(10),
         ];
     }
